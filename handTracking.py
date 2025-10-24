14555b3650c663c0bd9bc057d1923cace68cc845
Nezz77 <nesanduhm@gmail.com>
2025-10-24 19:34:07 +0530

    basic hand tracking

---
+++ handTracking.py
@@ -0,0 +1,50 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+prvTime = 0
+currTime = 0
+
+while True:
+    success, img = cap.read()
+    if not success:
+        break
+
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    #print(results.multi_hand_landmarks)
+    
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                #print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                print(id, cx, cy)
+                if id == 0:
+                    cv2.circle(img, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
+
+                mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    currTime = time.time()
+    fps = 1 / (currTime - prvTime) 
+    prvTime = currTime
+    cv2.putText(img, f'FPS: {int(fps)}', (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+
+    cv2.imshow("Webcam", img)
+
+    # Wait for key press (1 ms delay)
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
